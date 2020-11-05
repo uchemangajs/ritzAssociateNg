a1cc77555bb4d4c358132fe8d97e94f3e9dc54f3
Uchenna <uchemanga.js@gmail.com>
2020-11-05 12:02:55 +0100

    html css

---
+++ js/index.js
@@ -0,0 +1,25 @@
+"use strict"
+const $navUl = document.querySelector('[nav-ul');
+const $navCloseBtn = document.querySelector('[nav-close-btn]');
+const $menuNav = document.querySelector('[menu-nav]');
+const $barBtn = document.querySelector('[bar-btn]');
+
+$navCloseBtn.addEventListener('click', () => $menuNav.classList.add('hide'));
+
+$menuNav.addEventListener('click', () => $menuNav.classList.add('hide'));
+
+function toggleMenu (e) {
+    if($menuNav.classList.contains('hide')){
+       $menuNav.classList.remove('hide'); 
+    }else{
+
+        $menuNav.classList.add('hide');
+      console.log('togleissue')
+
+        
+      }
+       
+}
+
+$barBtn.addEventListener('click', toggleMenu);
+
