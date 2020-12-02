4d61bda9579b3118c6fa2901859ef8ca83807a00
Uchenna <uchemanga.js@gmail.com>
2020-12-02 17:01:46 +0100

    booking-form

--- js/car-lease.js
+++ js/car-lease.js
@@ -1,24 +1,6 @@
-'use strict'
-var slideIndex = 1;
-showSlides(slideIndex);
+"use strict"
+const bookingLink = document.querySelector('[booking-link]');
+const bookingForm = document.querySelector('[booking-form]');
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  
-  slides[slideIndex-1].style.display = "block";  
-}
+bookingLink.addEventListener('click', () => {bookingForm.classList.toggle("hide")
+console.log('hit')});
